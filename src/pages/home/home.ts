ae6e47cc63ea91dc1fbc8901ff3ca0c110d91d3d
Rubson Lima <limarubson@hotmail.com>
2017-10-16 12:24:50 -0300

    Funcionalidades do Fomulário Simples

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,23 +8,27 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class HomePage {
 
+  //Variável que recebe a biblioteca FormGroup
   private form: FormGroup;
 
   constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
 
+    //Variável que recebe a biblioteca 'formBuilder' com um grupo de informações
     this.form = this.formBuilder.group({
-
+      //E-mail com validação
       email: ['', Validators.required],
+      //Nome com validação
       nome: ['', Validators.required],
+      //Mensagem sem validação
       mensagem: [''],
 
     });
 
   }
 
+  //Método que executa no console as informações do formulário
   enviar() {
-    console.log(this.form.value)
+    console.log(this.form.value);
   }
-
 
 }
